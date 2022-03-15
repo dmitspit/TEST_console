95554c99c4c44403af94c2a827d04d3271a947fb
Dmytro <dmitspit@gmail.com>
2022-03-15 21:32:15 +0200

    Created branch feature

--- TEST_console/Program.cs
+++ TEST_console/Program.cs
@@ -7,6 +7,7 @@ namespace TEST_console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Faeture branch");
         }
     }
 }
